3c449588680de8726d9b87f54c195f0e8d03ed2f
Muhammad Afzal <mharis.javed1996@gmail.com>
2023-06-02 16:24:52 +0500

    completed login and registration apis

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,11 +5,21 @@ const router = express.Router();
 const isAuth = require('../middleware/is-auth');
 
 router.post('/login',[
-    body('email').notEmpty().escape().withMessage('Email Required'),
-    body('password').notEmpty().escape().withMessage('Password Required')
+    body('email').notEmpty().escape().isEmail().normalizeEmail().withMessage("Invalid Email Value"),
+    body('password').notEmpty().escape().withMessage('Password is Required')
 ], authController.login);
-// adding dummy Admin
-router.post('/dummyAdmin', authController.dummyAdmin);
+// registration
+router.post('/registration',[
+    body('name').notEmpty().escape().withMessage("Name is Required"),
+    body('email').notEmpty().escape().isEmail().normalizeEmail().withMessage("Invalid Email Value"),
+    body('password').notEmpty().escape().withMessage('Password is Required').custom((value, { req })=>{
+        if (value != req.body.cpassword) {
+            throw new Error('Passwords do not match with Confirm Password');
+        }
+        return true
+    }),
+    body('cpassword').notEmpty().escape().withMessage('Confirm Password is Required')
+] ,authController.registration);
 // XXXXXXXXXXXXXXXXXXXX
 router.post('/logout', isAuth, authController.logout);
 
